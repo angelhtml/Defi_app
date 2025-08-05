c62f345bec734c6b634e489c39591f00f8dc68fb
angelhtml <khfarhad6@gmail.com>
2025-08-05 06:06:29 +0330

    components

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,18 +2,27 @@ import MainStyle from "./Main.module.css";
 
 export default function Main(){
     return(
-        <div className={MainStyle.container}>
+        <div>
 
-            <div className={MainStyle.box1}>
-                <h2>Staking Balance</h2>
-                <div className={MainStyle.line}></div>
-                <h3>USDT</h3>
+            <div className={MainStyle.container}>
+                <div className={MainStyle.box1}>
+                    <h2>Staking Balance</h2>
+                    <div className={MainStyle.line}></div>
+                    <h3>USDT</h3>
+                </div>
+
+                <div className={MainStyle.box2}>
+                    <h2>Reward Balance</h2>
+                    <div className={MainStyle.line}></div>
+                    <h3>RWD</h3>
+                </div>
             </div>
 
-            <div className={MainStyle.box2}>
-                <h2>Reward Balance</h2>
-                <div className={MainStyle.line}></div>
-                <h3>RWD</h3>
+            <div className={MainStyle.main}>
+                <div className={MainStyle.dashboard}>
+                    <p>Stake Tokens</p>
+                    <p>Balance: </p>
+                </div>
             </div>
 
         </div>
